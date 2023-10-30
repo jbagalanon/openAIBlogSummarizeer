80815ecd9e4df63102f25c82de6b9cbba18cbe5b
Joemar - Server <jbagalanon@gmail.com>
2023-10-31 00:07:13 +0800

    Cherio web based not working

--- summarizer.js
+++ summarizer.js
@@ -1,5 +1,17 @@
-import {CherrioWebBasedLoader} from "langchain/document_loaders/web/cheerio";
+import { CherrioWebBasedLoader } from "langchain/document_loaders/web/cheerio";
 
-import {OpenAI} from "langchain/llms/openai";
+import { OpenAI } from "langchain/llms/openai";
 
-const loader =
+const loader = new CherrioWebBasedLoader(
+  "https://www.thephdefense.com/2023/05/enhancing-philippine-air-force-with-more-c-130/"
+);
+
+const data = await loader.load();
+
+const llm = new OpenAI();
+
+const query = "Please summarize the following data: ${data[0].pageContent}";
+
+const result = await llm.query(query);
+
+console.log(result);
